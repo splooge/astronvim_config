018dc54bc214220591476818275b379fe1c99a6a
Chris Stevens <chris@pwned.com>
2023-04-13 22:15:52 -0700

    Modify some noice stuff

--- plugins/user.lua
+++ plugins/user.lua
@@ -28,6 +28,14 @@ return {
       "rcarriga/nvim-notify",
     },
     event = "User AstroFile",
-    opts = {},
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+    },
   },
 }
